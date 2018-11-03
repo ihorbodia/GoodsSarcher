078e2b0d4cef96347ba5cac14eda3223482cd86a
Ihor <ihorbodia@gmail.com>
2018-11-03 18:27:51 +0100

    Fix fileshelper.

---
+++ GoodsSearcher/GoodsSearcher.Common/Helpers/FilesHelper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoodsSearcher.Common.Helpers
+{
+	public static class FilesHelper
+	{
+		public static string SelectFile()
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+
+			string selectedFileName = string.Empty;
+			if (openFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				selectedFileName = openFileDialog.FileName;
+			}
+			else
+			{
+				selectedFileName = string.Empty;
+			}
+			return selectedFileName;
+		}
+
+		public static string SelectFolder()
+		{
+			FolderBrowserDialog openFolderDialog = new FolderBrowserDialog();
+
+			string selectedFolderName = string.Empty;
+			if (openFolderDialog.ShowDialog() == DialogResult.OK)
+			{
+				selectedFolderName = openFolderDialog.SelectedPath;
+			}
+			else
+			{
+				selectedFolderName = string.Empty;
+			}
+			return selectedFolderName;
+		}
+
+		public static DataTable ConvertCSVtoDataTable(string strFilePath)
+		{
+			StreamReader sr = new StreamReader(strFilePath);
+			string[] headers = sr.ReadLine().Split(',');
+			DataTable dt = new DataTable();
+			foreach (string header in headers)
+			{
+				dt.Columns.Add(header);
+			}
+			string tab = '\u0009'.ToString();
+			string previousRow;
+			while (!sr.EndOfStream)
+			{
+				var currentRow = sr.ReadLine();
+				if (currentRow.StartsWith(tab))
+				{
+
+				}
+				else
+				{
+
+				}
+				previousRow = sr.ReadLine();
+				string[] rows = Regex.Split(sr.ReadLine(), ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+				DataRow dr = dt.NewRow();
+				for (int i = 0; i < headers.Length; i++)
+				{
+					dr[i] = rows[i];
+				}
+
+				dt.Rows.Add(dr);
+			}
+			return dt;
+		}
+	}
+}
